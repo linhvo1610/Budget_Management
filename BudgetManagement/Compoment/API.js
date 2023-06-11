617b942393eb2dcadc4bea15270dcca3ddb4325f
linhvo1610 <lancelot.g1874@gmail.com>
2023-06-11 09:32:57 +0700

    First balance commit

--- BudgetManagement/Compoment/API.js
+++ BudgetManagement/Compoment/API.js
@@ -1,4 +1,6 @@
 export const API={
     login:'http://192.168.102.12:8000/api/users?username=',
     register:'http://192.168.102.12:8000/api/users',
+    getbalance:'http://192.168.102.12:8000/api/balance?id_user=',
+    updatebalance:'http://192.168.102.12:8000/api/balance/'
 }
