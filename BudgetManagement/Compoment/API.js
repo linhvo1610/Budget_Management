da0084fec73d7a361af896b7d9e1b2f9d2572e0c
linhvo1610 <lancelot.g1874@gmail.com>
2023-06-19 14:16:42 +0700

    API adjust

--- BudgetManagement/Compoment/API.js
+++ BudgetManagement/Compoment/API.js
@@ -1,11 +1,13 @@
 export const API = {
-    login: 'http://192.168.2.140:8000/api/users?username=',
-    register: 'http://192.168.2.140:8000/api/users',
-    getbalance: 'http://192.168.2.140:8000/api/balance?id_user=',
-    updatebalance: 'http://192.168.2.140:8000/api/balance/',
-    getrecord: 'http://192.168.2.140:8000/api/record?id_user=',
-    deleterecord: 'http://192.168.2.140:8000/api/record/',
-    updaterecord: 'http://192.168.2.140:8000/api/record/'
+    login: 'http://192.168.102.12:8000/api/users?username=',
+    register: 'http://192.168.102.12:8000/api/users',
+    getbalance: 'http://192.168.102.12:8000/api/balance?id_user=',
+    addbalance: 'http://192.168.102.12:8000/api/balance',
+    updatebalance: 'http://192.168.102.12:8000/api/balance/',
+    getrecord: 'http://192.168.102.12:8000/api/record?id_user=',
+    deleterecord: 'http://192.168.102.12:8000/api/record/',
+    updaterecord: 'http://192.168.102.12:8000/api/record/',
+    getcategory:'http://192.168.102.12:8000/api/category'
 
 
 }
